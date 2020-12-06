b62acc25665ce2c734a2877d4a46684cebf6d4be
matt989253 <matthieu.burguburu@gmail.com>
2020-12-06 21:18:10 +0100

    Add basic watcher using Selenium

---
+++ runme.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+python -m pip install schedule
+# usage: python script.py <digitec url> <waiting time (seconds)> <headless mode(true/false)>
+python script.py "https://www.digitec.ch/fr/s1/product/amd-ryzen-9-5900x-am4-370ghz-12-core-processeurs-13987917" 60 true
