f208f84c510ca7344ab9dbb3fb5869758e5d8a9e
matt989253 <matthieu.burguburu@gmail.com>
2020-12-07 11:47:20 +0100

    Use async tasks to check multiple products at once

--- runme.bat
+++ runme.bat
@@ -1,3 +1,2 @@
 
-pip install colorama
-python .\soup.py "https://www.digitec.ch/fr/s1/product/zotac-geforce-rtx-3080-trinity-oc-10go-cartes-graphiques-13874271?tagIds=76" 10
+python .\soup.py
--- soup.py
+++ soup.py
@@ -1,4 +1,5 @@
 import requests
+import asyncio
 import time
 import json
 import sys
@@ -7,20 +8,39 @@ from colorama import init, Fore, Back, Style
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-url = sys.argv[1] # product url
-wait = int(sys.argv[2]) # time to wait in secconds
+async def check(name, url, wait):
+    while True:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-init()
+        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")    
+        price = soup.select('html body div#__next div div div main#pageContent div div div div div.productDetail div span strong')[0].text.strip()
+        availability = soup.select('html body div div div div main#pageContent div div div div div.productDetail div span span span.availabilityText div div')[0].text
 
-while True:
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+        print ("%s - %s (%s) - %s%s%s" % (now, name, price, Fore.RED + Style.BRIGHT, availability, Style.RESET_ALL), flush = True)
+        
+        await asyncio.sleep(wait)
 
-    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")    
-    name = soup.select('html body div#__next div div div main#pageContent div div div div div.productDetail h1')[0].text
+# ===================================================================================================================================================================== #
 
-    availability = soup.select('html body div div div div main#pageContent div div div div div.productDetail div span span span.availabilityText div div')[0].text
-    price = soup.select('html body div#__next div div div main#pageContent div div div div div.productDetail div span strong')[0].text.strip()
+async def main():
+    items = [
+            ("5900x", "https://www.digitec.ch/fr/s1/product/amd-ryzen-9-5900x-am4-370ghz-12-core-processeurs-13987917"),
+            ("3080 STRIX", "https://www.digitec.ch/fr/s1/product/asus-geforce-rog-strix-rtx-3080-o10g-gaming-10go-cartes-graphiques-13751783"),
+            ]
+    wait = 60
 
-    print ("%s - %s (%s) - %s%s%s" % (now, name, price, Fore.RED + Style.BRIGHT, availability, Style.RESET_ALL), flush = True)
-    time.sleep(wait)
+    tasks = []
+
+    init()
+    for item in items:
+        tasks.append(asyncio.create_task(check(item[0], item[1], wait)))
+
+    for task in tasks:
+        await task
+
+    print ("Successfully terminated")
+
+# ===================================================================================================================================================================== #
+
+asyncio.run(main())
