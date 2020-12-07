17e237e156de07835a4f4d41e6dd1d5982e4f7b8
matt989253 <matthieu.burguburu@gmail.com>
2020-12-07 12:28:42 +0100

    Add alert sound

--- soup.py
+++ soup.py
@@ -1,3 +1,4 @@
+import winsound
 import requests
 import asyncio
 import time
@@ -8,17 +9,39 @@ from colorama import init, Fore, Back, Style
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+def setColor(string, color):
+    return color + string + Style.RESET_ALL
+
 async def check(name, url, wait):
     while True:
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
 
-        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")    
-        price = soup.select('html body div#__next div div div main#pageContent div div div div div.productDetail div span strong')[0].text.strip()
+        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+        price = soup.select('html body div#__next div div div main#pageContent div div div div div.productDetail div strong')[0].text.strip()
+        price = setColor(price, Fore.CYAN)
+
         availability = soup.select('html body div div div div main#pageContent div div div div div.productDetail div span span span.availabilityText div div')[0].text
 
-        print ("%s - %s (%s) - %s%s%s" % (now, name, price, Fore.RED + Style.BRIGHT, availability, Style.RESET_ALL), flush = True)
-        
+        color = ""
+        if "Livré" in availability:
+            winsound.PlaySound("alert.wav", winsound.SND_FILENAME)
+            color = Fore.GREEN
+        elif("Entre") in availability:
+            winsound.PlaySound("alert.wav", winsound.SND_FILENAME)
+            color = Fore.CYAN
+        elif "Non disponible" in availability:
+            color = Fore.RED
+        elif("Nous clarifions") in availability:
+            color = Fore.RED
+        else:
+             color = Fore.YELLOW
+        color += Style.BRIGHT
+
+        availability = setColor(availability, color)
+
+        print ("%s - %s (%s) - %s" % (now, name, price, availability), flush = True)
         await asyncio.sleep(wait)
 
 # ===================================================================================================================================================================== #
@@ -28,7 +51,7 @@ async def main():
             ("5900x", "https://www.digitec.ch/fr/s1/product/amd-ryzen-9-5900x-am4-370ghz-12-core-processeurs-13987917"),
             ("3080 STRIX", "https://www.digitec.ch/fr/s1/product/asus-geforce-rog-strix-rtx-3080-o10g-gaming-10go-cartes-graphiques-13751783"),
             ]
-    wait = 60
+    wait = 10
 
     tasks = []
 
